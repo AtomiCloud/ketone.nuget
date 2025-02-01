5edc010fb478cfd04d43a83fcdbb6e0dfbaca00f
kirinnee <kirinnee97@gmail.com>
2025-02-01 15:45:45 +0800

    feat: include Result Helper for tests

---
+++ template/UnitTest/Initializer.cs
@@ -0,0 +1,18 @@
+using CarboxylicLithiumTestHelper;
+using FluentAssertions;
+using FluentAssertions.Extensibility;
+using UnitTest;
+
+[assembly: AssertionEngineInitializer(typeof(Initializer), nameof(Initializer.Initialize))]
+
+namespace UnitTest;
+
+public static class Initializer
+{
+    public static void Initialize()
+    {
+        AssertionConfiguration.Current.Equivalency.Modify(o =>
+            o.Using(new ResultEquivalencyStep())
+        );
+    }
+}
